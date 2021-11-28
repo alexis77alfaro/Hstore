4a86f664bca810fee51a57a6efd88b57d057cc4b
alexis77alfaro <47479100+alexis77alfaro@users.noreply.github.com>
2021-11-28 13:31:19 -0600

    push crud productos

--- HardwareStore/HardwareStore/Controllers/ProductosController.cs
+++ HardwareStore/HardwareStore/Controllers/ProductosController.cs
@@ -90,6 +90,7 @@ namespace HardwareStore.Controllers
         //Método GET para AgregarProducto(Retorna la View con el Form para Agregar)
         public IActionResult AgregarProducto()
         {
+
             if (HttpContext.Session.GetString("IdUsuario") != null)
             {
                 string id = HttpContext.Session.GetString("IdUsuario");
@@ -124,25 +125,74 @@ namespace HardwareStore.Controllers
             }
         }
 
-        //Método GET para EditarProducto(Retorna la View con el Form para Editar)
-        public IActionResult EditarProducto()
+
+        [HttpPost]
+        public IActionResult AgregarProductop(Producto pr)
         {
+
+            if(ModelState.IsValid)
+            {
+                _context.producto.Add(pr);
+                _context.SaveChanges();
+
+                TempData["Mensaje"] = "Producto agregado";
+                return RedirectToAction( "IndexAdmin","Productos");
+
+            }
+
+            return View(); 
+        }
+
+
+
+
+
+        //Método GET para EditarProducto(Retorna la View con el Form para Editar)
+        public IActionResult EditarProducto(int? ID)
+        {
+
+
             if (HttpContext.Session.GetString("IdUsuario") != null)
             {
+
                 string id = HttpContext.Session.GetString("IdUsuario");
 
                 SesionUsuario = new Usuario();
                 SesionUsuario.Id = int.Parse(id);
                 SesionUsuario = _context.usuario.Where(s => s.Id == SesionUsuario.Id).FirstOrDefault();
 
+                
+  
+
+
                 try
                 {
+
                     ViewBag.Id = SesionUsuario.Id;
                     ViewBag.Nombre = SesionUsuario.Nombre;
                     ViewBag.Correo = SesionUsuario.Correo;
                     ViewBag.Rol = SesionUsuario.Rol;
+
+                    if (ID == null || ID == 0)
+                    {
+                        return NotFound();
+                    }
+
+                    //obtener id de los productos
+
+                    var pro = _context.producto.Find(ID);
+
+                    if (pro == null)
+                    {
+                        return NotFound();
+                    }
+                    return View(pro);
+
                 }
                 catch { }
+
+              
+
 
                 if (SesionUsuario.Rol == 0)
                 {
@@ -159,7 +209,129 @@ namespace HardwareStore.Controllers
                 SesionUsuario = new Usuario();
                 return RedirectToAction("ErrorUsuario");
             }
+
+
+          
+
+
+
         }
+
+
+        //Método POST para EditarProducto Interactua con la base 
+        [HttpPost]
+        public IActionResult EditarProducto(Producto p)
+         {
+
+            if (ModelState.IsValid)
+            {
+                _context.producto.Update(p);
+                _context.SaveChanges();
+
+                TempData["Mensaje"] = "Producto actualizado";
+                return RedirectToAction("IndexAdmin", "Productos");
+
+            }
+
+            return RedirectToAction("IndexAdmin", "Productos");
+        }
+
+        //Método GET Elimarproducto
+        public IActionResult EliminarProducto(int? ID)
+        {
+
+            if (HttpContext.Session.GetString("IdUsuario") != null)
+            {
+
+                string id = HttpContext.Session.GetString("IdUsuario");
+
+                SesionUsuario = new Usuario();
+                SesionUsuario.Id = int.Parse(id);
+                SesionUsuario = _context.usuario.Where(s => s.Id == SesionUsuario.Id).FirstOrDefault();
+
+
+
+
+
+                try
+                {
+
+                    ViewBag.Id = SesionUsuario.Id;
+                    ViewBag.Nombre = SesionUsuario.Nombre;
+                    ViewBag.Correo = SesionUsuario.Correo;
+                    ViewBag.Rol = SesionUsuario.Rol;
+
+                    if (ID == null || ID == 0)
+                    {
+                        return NotFound();
+                    }
+
+                    //obtener id de los productos
+
+                    var pro = _context.producto.Find(ID);
+
+                    if (pro == null)
+                    {
+                        return NotFound();
+                    }
+                    return View(pro);
+
+                }
+                catch { }
+
+
+
+
+                if (SesionUsuario.Rol == 0)
+                {
+                    //Codigo que ejecuta este metodo aquí
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("ErrorSoloAdmin");
+                }
+            }
+            else
+            {
+                SesionUsuario = new Usuario();
+                return RedirectToAction("ErrorUsuario");
+            }
+
+        }
+
+
+        //Metodo POST ELimarProducto 
+
+        [HttpPost]
+        public IActionResult EliminarProductos(int? ID)
+        {
+            //Obtener el Producto por el id para eliminar
+
+            var p = _context.producto.Find(ID);
+
+            if(p==null)
+            {
+                return NotFound();
+            }
+
+
+            
+                _context.producto.Remove(p);
+                _context.SaveChanges();
+
+                TempData["Mensaje"] = "Producto actualizado";
+                return RedirectToAction("IndexAdmin", "Productos");
+
+            
+
+            
+
+
+        }
+
+
+
 
     }
 }
